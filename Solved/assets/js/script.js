12481d066ac8dd2e00f56e6af969944f1d375111
Byungwook Yoon <byung90@gmail.com>
2021-03-02 21:55:53 -0500

    done

---
+++ Solved/assets/js/script.js
@@ -0,0 +1,27 @@
+var userContainer = document.getElementById('users');
+var fetchButton = document.getElementById('fetch-button');
+
+function getApi() {
+  var requestUrl = 'https://api.github.com/users?per_page=5';
+
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      // Use the console to examine the response
+      console.log(data);
+      // TODO: Loop through the data and generate your HTML
+      for (var i = 0; i < data.length; i++) {
+        let userNameEl = document.createElement('h3');
+        let urlEl = document.createElement('p');
+
+        userNameEl.textContent = data[i].login;
+        urlEl.textContent = data[i].html_url;
+
+        userContainer.appendChild(userNameEl);
+        userContainer.appendChild(urlEl);
+      }
+    });
+}
+fetchButton.addEventListener('click', getApi);
